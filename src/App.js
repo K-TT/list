0526c28d06635c3f0d37f0a27f62d4a3475ad741
Kateryna <katerinakhomenko93@gmail.com>
2022-02-10 23:04:06 -0500

    added search through the user list function

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
 import './App.css';
-import Table from "./Table";
-import { Container, Header } from "semantic-ui-react";
-import {Component} from "react";
+import { Container, Header, Input, Tab} from "semantic-ui-react";
+import React, {Component} from "react";
+import Users from './users.json'
+
 
 class App extends Component{
   constructor(props) {
     super(props);
-    this.state={}
+    this.state={
+      name:""
+    }
   }
+
+
   render(){
+
+    let panes = Users.data.filter((user)=>{
+        if(user.username.toLowerCase().includes(this.state.name.toLowerCase())){
+          return user;
+        }
+
+    }).map(
+      (user)=>({
+        menuItem: user.username,
+        render: () => <Tab.Pane><div>
+          <h1>{user.username}. {user.position}</h1>
+          <h2>{user.company}</h2>
+          <p>{user.skills.join(", ")}</p>
+        </div></Tab.Pane>
+      })
+    );
+
+    const Table =()=>(	<Tab menu={{ fluid: true, vertical: true }}
+                             menuPosition='left'
+                             panes={panes} />)
 
     return(
       <Container style={{ margin: 20 }}>
         <Header as="h3">The List of the Users</Header>
-
-     <Table />
+        <Input name="search" icon='users' iconPosition='left' placeholder='Search users...' onChange={(e)=>{this.setState({name:e.target.value})}}/>
+        <br/>  <br/>
+      <Table />
       </Container>
     );
   }}
